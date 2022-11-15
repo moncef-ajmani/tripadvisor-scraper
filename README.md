be438027a6e8f31484b1e422dcf90c48f3301f19
moncef_ajmani <moncef.ajmani@gmail.com>
2022-11-16 00:20:05 +0100

    Update

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# Tripadvisor-Hotels-Reviews
-Tripadvisor Hotel Reviews with Rating 
+#Tripadvisor scrapper
+
+This is a scrapper that scrapes reviews from tripadvisor.
+
+##Requirements
+
+- Python 3.7.4
+- BeautifulSoup 4
+- requests 2.22.0
+- csv
+- termcolor
+
+##Usage
+
+- Run the following command to install the required libraries:
+
+pip install -r requirements.txt
+
+- Run the scrapper.py file with the following command:
+
+python scrapper.py <url> <filename> [<max_reviews_per_hotel>]
+
+- <url>: The URL of the search results page on tripadvisor.
+- <filename>: The name of the CSV file that will be created.
+- <max_reviews_per_hotel>: (optional) The maximum number of reviews to be scraped from each hotel. If not specified, all reviews will be scraped.
+
+##Example
+
+python scrapper.py https://www.tripadvisor.com/Hotels-g293732-Casablanca_Casablanca_Settat-Hotels.html casablanca_hotels_reviews 10
+
+This will scrape reviews from all hotels reviews
---
+++ scrapper.py
@@ -0,0 +1,95 @@
+# import required libraries
+import requests
+from bs4 import BeautifulSoup
+import re
+import math 
+import csv
+import time
+import sys 
+from termcolor import colored
+
+# define headers variable to be used in all requests
+headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
+
+reviews_count=0
+
+# define a function get_number_pages that takes an url as a parameter and return the number of pages of search results
+def get_number_pages(url):
+    r = requests.get(url, headers=headers)
+    soup = BeautifulSoup(r.content,'html5lib')
+    number_pages = math.ceil(int(re.findall("^\d*",soup.find('span',{"class":"qrwtg"}).text)[0]) / 30)
+    return number_pages
+
+
+# define a function get_hotels_links_reviews_count that takes an url as a parameter and return a list of links of all hotels
+def get_hotels_links_reviews_count(url):
+    data = []
+    # get all hotels links with reviews count
+    for i in range(0,get_number_pages(url)):
+        if i!=0:
+            url = re.split("-",url)
+            url.insert(2,f"oa{i*30}")
+            url = "-".join(url)
+        
+        r = requests.get(url, headers=headers)
+        soup = BeautifulSoup(r.content,'html5lib')
+        for row in soup.find_all("div",{"class":"listing"}):
+            obj = {}
+            obj["link"] = row.find("div",{"class":"listing_title"}).a['href']
+            obj["reviews_count"] = int(re.split(" ",row.find("a",{"class":"review_count"}).text)[0].replace(",",""))
+            data.append(obj)
+    print(f"{colored(len(data), 'green')} hotels founded.")
+
+    return data
+# define a function get_reviews_from_hotel that takes an url as a parameter and return a list of reviews
+def get_reviews_from_hotel(url,number_reviews,max_reviews):
+    global reviews_count
+    reviews = []
+    pages_count = number_reviews
+    if(max_reviews):
+        pages_count = int((max_reviews/10))+1
+    
+    for i in range(0,pages_count):
+        if url!=0:
+            url = re.split("-",url)
+            url.insert(4,f'or{i*10}')
+            url = "-".join(url)
+
+        r = requests.get(url, headers=headers)
+        soup = BeautifulSoup(r.content,'html5lib')
+        for row in soup.find_all("div",{"class":"WAllg _T"}):
+            review = {}
+            review["text"]= row.find("q",{"class":"QewHA"}).text
+            review["rating"] = int(re.findall("\d*$",row.find("div",{"class":"Hlmiy"}).span["class"][1])[0])/10
+            reviews.append(review)
+    reviews_count += len(reviews)
+    return reviews
+
+# define a function get_reviews that takes 3 parameters (url,filename,max_reviews_per_hotel) and writes the scrapper data into a csv file
+def get_reviews(url,filename,max_reviews_per_hotel=0):
+    with open(f"{filename}.csv",'w',encoding="utf-8") as f:
+        links = get_hotels_links_reviews_count(url)
+        writer = csv.DictWriter(f, fieldnames = ["text","rating"])
+        writer.writeheader()
+        print("Start Scraping reviews...")
+        for row in links:
+            if row['reviews_count']==0:continue
+            reviews = get_reviews_from_hotel(f"https://www.tripadvisor.in{row['link']}",row['reviews_count'],max_reviews_per_hotel)
+            writer.writerows(reviews)
+
+            print(f"{colored(reviews_count, 'green')} reviews added Successfully.",end="\r")
+    
+
+# main Program 
+if __name__ == "__main__":
+    start_time = time.time()
+    print(colored("Start...\n",'blue'))
+
+    max_reviews = 0
+    if(len(sys.argv)>3):
+        max_reviews = sys.argv[3]
+
+    get_reviews(sys.argv[1],sys.argv[2],max_reviews)
+
+    print(colored("\nDone :)","blue"))
+    print(colored("--- %s seconds ---"%(time.time() - start_time),"yellow"))
